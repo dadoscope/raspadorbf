f320ddb791d300ff83a3bc6196145799cf1bbe73
cndesantana <charles.santana@gmail.com>
2020-02-15 18:12:08 +0100

    Update scraper

--- R/raspador.R
+++ R/raspador.R
@@ -4,7 +4,7 @@ base_address<- "http://transparencia.gov.br/download-de-dados/bolsa-familia-paga
 
 #Alterar as variáveis abaixo de acordo com o que se deseja
 ano_ini<- 2013
-ano_fim<- 2019
+ano_fim<- 2013
 mes_ini<-1
 mes_fim<-12
 ult_mes_disp<- 1
@@ -14,6 +14,6 @@ for (ano in ano_ini:ano_fim){
   for (mes in mes_ini:mes_fim){
     mes<- ifelse(mes>=10,as.character(mes),paste0('0',mes))
     arq_down<- paste0(base_address,ano,mes)
-    download.file(arq_down,destfile = paste0("../data/bf_",ano,mes,'_BF','.zip'), mode='wb')
+    download.file(arq_down,destfile = paste0("../data/",ano,mes,'_BF','.zip'), mode='wb')
   }
 }
